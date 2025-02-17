caaefc06cd35ce91059cf5bd00595171587db7b8
MohammedDigitalSage <159501876+MohammedDigitalSage@users.noreply.github.com>
2025-02-16 21:32:23 -0500

    Update submission.cpp

--- submission.cpp
+++ submission.cpp
@@ -12,20 +12,33 @@ e
 y
 */
 
+/*
+Author: Mohammed Uddin
+Course: CSCI-135
+Assignment: E4.8
+
+A program that reads a word and prints each character on a separate line.
+
+User Input: "Hey"
+Output:
+H
+e
+y
+*/
+
 #include <iostream>
 using namespace std;
 
 int main()
 {
   string word_taker;
-  int word_lenght;
-  
+
   cout << "Enter a word: ";
   getline(cin, word_taker);
-  word_lenght = word_taker.size();
 
-  for(int i = 0; i < word_taker.size(); i++) {
+  for (int i = 0; i < word_taker.size(); i++) {
     cout << word_taker[i] << "\n";
   }
 
-return 0;
+  return 0;
+}
